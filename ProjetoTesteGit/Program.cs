7f6674a53851c41ad165d49bf84d098a04c2f275
xPedroV98 <pedrovteixeira98@gmail.com>
2019-01-27 11:40:49 -0200

    Projeto Inicial

---
+++ ProjetoTesteGit/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ProjetoTesteGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine();
+            Console.WriteLine("Bom dia: ");
+            Console.WriteLine("Tenha um ótimo dia!!!");
+
+        }
+    }
+}
